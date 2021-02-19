5452010ae4184a61882efdf6f41af62c88b4039c
Gelios <Geliosal39hon@gmail.com>
2021-02-19 14:37:59 +0300

    first section

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,13 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.menu');
+    const closeBtn = document.querySelector('.menu__close');
+    const hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        menu.classList.add('active');
+    });
+
+    closeBtn.addEventListener('click', () => {
+        menu.classList.remove('active');
+    });
 });
